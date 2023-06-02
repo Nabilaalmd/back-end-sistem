a1c014bb2bf5fa2be04e6025e000d382f11e2b90
dms-pcm <81421412+dms-pcm@users.noreply.github.com>
2023-06-02 13:29:02 +0700

    update ke - 2

--- routes.js
+++ routes.js
@@ -1,39 +1,39 @@
 const routes = [
-    {
-        method: '*',
-        path: '/',
-        handler: (request, h) => {
-            return 'Halaman tidak dapat diakses dengan method tersebut';
-        },
+  {
+    method: "*",
+    path: "/",
+    handler: (request, h) => {
+      return "Halaman tidak dapat diakses dengan method tersebut";
     },
-    {
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-            return 'Homepage';
-        },
+  },
+  {
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return "Homepage 2023";
     },
-    {
-        method: 'GET',
-        path: '/about',
-        handler: (request, h) => {
-            return 'About page';
-        },
+  },
+  {
+    method: "GET",
+    path: "/about",
+    handler: (request, h) => {
+      return "About page";
     },
-    {
-        method: 'GET',
-        path: '/test',
-        handler: (request, h) => {
-            return 'Halaman tidak ditemukan';
-        },
+  },
+  {
+    method: "GET",
+    path: "/test",
+    handler: (request, h) => {
+      return "Halaman tidak ditemukan";
     },
-    {
-        method: 'POST',
-        path: '/',
-        handler: (request, h) => {
-            return 'Halaman tidak dapat diakses dengan method tersebut';
-        },
+  },
+  {
+    method: "POST",
+    path: "/",
+    handler: (request, h) => {
+      return "Halaman tidak dapat diakses dengan method tersebut";
     },
+  },
 ];
 
 module.exports = routes;
